b51bd5259a8ea233f1d04bdac0cc2135dbb967ee
Djamal <nyarly51@gmail.com>
2020-11-03 22:13:29 +0300

    Refactor fixture loader

--- src/bin/fixtures/loadAuthors.ts
+++ src/bin/fixtures/loadAuthors.ts
@@ -1,16 +1,39 @@
 import '../../bootstrap';
-import { initContainer  } from 'components/di';
-import { resolve } from 'path';
-
 import Container from 'typedi';
+import { resolve } from 'path';
+import { plainToClass } from 'class-transformer';
+
+import { initContainer  } from 'components/di';
 import { Type } from 'components/container/Type';
+
 import { AuthorRepository } from 'inf/AuthorRepository';
+import { BookRepository } from 'inf/BookRepository';
+
+import { Author } from 'domain/Author';
+import { Book } from 'domain/Book';
+
+const saveAuthorData = async (
+  data: { name: string; books: {name: string; pageCount: number;}[]; },
+  authorRepository: AuthorRepository,
+  bookRepository: BookRepository
+) => {
+  const author = plainToClass(Author, data);
+  await authorRepository.save(author);
+  await Promise.all(
+    data.books
+      .map(book => bookRepository.save(
+        plainToClass(Book, { ...book, authorId: author.authorId }))
+      )
+  );
+}
 
 initContainer().then(async() => {
   const fixturePath = resolve(__dirname, '../../../fixtures/authors');
-  const repository = Container.get<AuthorRepository>(Type.AuthorRepository);
+
+  const bookRepository = Container.get<BookRepository>(Type.BookRepository);
+  const authorRepository = Container.get<AuthorRepository>(Type.AuthorRepository);
 
   const data = require(fixturePath);
-
-  await repository.save(data);
+  await Promise.all(data.map((it: any) => saveAuthorData(it, authorRepository, bookRepository)));
 })
+
